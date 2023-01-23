5f057987535f130de89150a1c407ba70d0facae4
Gokhankra <gokhankra0@gmail.com>
2023-01-23 13:41:52 -0500

    lastversion

--- src/test/java/InvoiceProject/runner/TestRunner.java
+++ src/test/java/InvoiceProject/runner/TestRunner.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
         glue = "InvoiceProject/stepDefinitions",
         tags = "",
         dryRun = false,
+        publish = true,
         plugin = {
                 "pretty",
                 "rerun:target/rerun.txt",
