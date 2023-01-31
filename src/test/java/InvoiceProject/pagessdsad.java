a12cf6726fd4c0c1bace057cb7c666cd858f070c
Gokhankra <gokhankra0@gmail.com>
2023-01-30 20:11:50 -0500

    S

--- src/test/java/InvoiceProject/pagessdsad.java
+++ src/test/java/InvoiceProject/pagessdsad.java
@@ -1,13 +1,9 @@
 package InvoiceProject;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class pagessdsad {
 
     public static void main(String[] args) {
 
-
+        System.out.println("hello");
     }
 }
